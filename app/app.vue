7bc67ac2b50e93a3982f738c6e209960cf54add3
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-01-22 18:50:23 +0200

    first commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="bg-red-300 text-center">hi</h1>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-red-300 text-center font-bold text-6xl">
+    hi in index page
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
