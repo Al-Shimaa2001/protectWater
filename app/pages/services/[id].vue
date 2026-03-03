b651a5bdf10ead42bf8417e8a7be6af448036093
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-03-03 22:39:16 +0200

    update offer section

--- app/pages/services/[id].vue
+++ app/pages/services/[id].vue
@@ -54,13 +54,17 @@
           {{ price }}
         </p>
       </div>
+      <div class="text-red-600 font-bold text-2xl w-100 py-2 my-3">
+        عروض خاصة %
+      </div>
       <div
         v-for="(offer, index) in offers"
         :key="index"
         class="text-sm flex justify-start items-start gap-2 my-2"
       >
         <UIcon :name="offer.icon" class="w-5 h-5 text-blue-700 shrink-0" />
-        {{ offer.details }}
+
+        <p>{{ offer.details }}</p>
       </div>
       <div class="flex justify-center items-center mt-5">
         <ButtonContactUs />
@@ -77,12 +81,13 @@ const price = "احصل على استشارة وعرض سعر مخصص";
 const offers = [
   {
     icon: "heroicons:sparkles",
-    details: " المساجد الكبيرة و للجمعيات الخيرية خصم خاص  في جميع الخدمات",
+    title: "خصم خاص ",
+    details: "     المساجد الكبيرة و للجمعيات الخيرية  في جميع الخدمات",
   },
   {
     icon: "heroicons:sparkles",
-    details:
-      "  للمساحات الكبيرة و للجمعيات الخيرية خصم خاص  في جميع انواع العزل",
+    title: "خصم خاص ",
+    details: "     للمساحات الكبيرة و للجمعيات الخيرية في جميع انواع العزل",
   },
 ];
 
