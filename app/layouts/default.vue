df1a21168bc78d33da41b17397cc14a1b58b1fe6
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-03-06 16:13:20 +0200

    add speedInsights vercel

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import MobileNav from "~/components/Nav/MobileNav.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 </script>
 
 <template>
   <nav-bar class="hidden lg:flex" />
   <MobileNav class="flex lg:hidden" />
+  <SpeedInsights />
   <slot />
   <Footer />
 </template>
